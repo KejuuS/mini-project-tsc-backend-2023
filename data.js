2dd3d2508b3eba6535eadb23414e063ac8f276fa
KejuuS <111622556+KejuuS@users.noreply.github.com>
2023-10-22 22:52:44 +0700

    Menambahkan data buku

--- data.js
+++ data.js
@@ -1,8 +1,25 @@
 // data.js
 const libraryData = [
-    { title: "Buku 1", author: "Penulis 1" },
-    { title: "Buku 2", author: "Penulis 2" },
-    { title: "Buku 3", author: "Penulis 3" },
+    { title: "To Kill a Mockingbird", author: "Harper Lee" }
+    { title: "1984", author: "George Orwell" }
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" }
+    { title: "Pride and Prejudice", author: "Jane Austen" }
+    { title: "The Catcher in the Rye", author: "J.D. Salinger" }
+    { title: "The Hobbit", author: "J.R.R. Tolkien" }
+    { title: "The Hunger Games", author: "Suzanne Collins" }
+    { title: "The Da Vinci Code", author: "Dan Brown" }
+    { title: "Brave New World", author: "Aldous Huxley" }
+    { title: "The Lord of the Rings", author: "J.R.R. Tolkien" }
+    { title: "The Alchemist", author: "Paulo Coelho" }
+    { title: "Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling" }
+    { title: "The Girl with the Dragon Tattoo", author: "Stieg Larsson" }
+    { title: "The Shining", author: "Stephen King" }
+    { title: "Gone with the Wind", author: "Margaret Mitchell" }
+    { title: "To the Lighthouse", author: "Virginia Woolf" }
+    { title: "The Road", author: "Cormac McCarthy" }
+    { title: "Moby-Dick", author: "Herman Melville" }
+    { title: "A Tale of Two Cities", author: "Charles Dickens" }
+    { title: "War and Peace", author: "Leo Tolstoy" }
     // Tambahkan data buku lainnya sesuai kebutuhan
 ];
 

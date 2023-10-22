a1b2e9f9145453567406981c434dc9a31012c31d
KejuuS <111622556+KejuuS@users.noreply.github.com>
2023-10-22 22:53:44 +0700

    Menghilangkan bug

--- data.js
+++ data.js
@@ -1,24 +1,24 @@
 // data.js
 const libraryData = [
-    { title: "To Kill a Mockingbird", author: "Harper Lee" }
-    { title: "1984", author: "George Orwell" }
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" }
-    { title: "Pride and Prejudice", author: "Jane Austen" }
-    { title: "The Catcher in the Rye", author: "J.D. Salinger" }
-    { title: "The Hobbit", author: "J.R.R. Tolkien" }
-    { title: "The Hunger Games", author: "Suzanne Collins" }
-    { title: "The Da Vinci Code", author: "Dan Brown" }
-    { title: "Brave New World", author: "Aldous Huxley" }
-    { title: "The Lord of the Rings", author: "J.R.R. Tolkien" }
-    { title: "The Alchemist", author: "Paulo Coelho" }
-    { title: "Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling" }
-    { title: "The Girl with the Dragon Tattoo", author: "Stieg Larsson" }
-    { title: "The Shining", author: "Stephen King" }
-    { title: "Gone with the Wind", author: "Margaret Mitchell" }
-    { title: "To the Lighthouse", author: "Virginia Woolf" }
-    { title: "The Road", author: "Cormac McCarthy" }
-    { title: "Moby-Dick", author: "Herman Melville" }
-    { title: "A Tale of Two Cities", author: "Charles Dickens" }
+    { title: "To Kill a Mockingbird", author: "Harper Lee" },
+    { title: "1984", author: "George Orwell" },
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { title: "Pride and Prejudice", author: "Jane Austen" },
+    { title: "The Catcher in the Rye", author: "J.D. Salinger" },
+    { title: "The Hobbit", author: "J.R.R. Tolkien" },
+    { title: "The Hunger Games", author: "Suzanne Collins" },
+    { title: "The Da Vinci Code", author: "Dan Brown" },
+    { title: "Brave New World", author: "Aldous Huxley" },
+    { title: "The Lord of the Rings", author: "J.R.R. Tolkien" },
+    { title: "The Alchemist", author: "Paulo Coelho" },
+    { title: "Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling" },
+    { title: "The Girl with the Dragon Tattoo", author: "Stieg Larsson" },
+    { title: "The Shining", author: "Stephen King" },
+    { title: "Gone with the Wind", author: "Margaret Mitchell" },
+    { title: "To the Lighthouse", author: "Virginia Woolf" },
+    { title: "The Road", author: "Cormac McCarthy" },
+    { title: "Moby-Dick", author: "Herman Melville" },
+    { title: "A Tale of Two Cities", author: "Charles Dickens" },
     { title: "War and Peace", author: "Leo Tolstoy" }
     // Tambahkan data buku lainnya sesuai kebutuhan
 ];
